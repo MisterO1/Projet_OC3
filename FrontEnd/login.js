0304b8c66a2d75946c8f063bba6768d9d01db8d4
misterO <mistero4321@gmail.com>
2025-04-29 17:51:26 +0200

    feat : add login.js for connection

---
+++ FrontEnd/login.js
@@ -0,0 +1,42 @@
+// const submitBtn = document.querySelector("form button")
+const form = document.querySelector("form")
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const inputs = document.querySelectorAll("input")
+    const email = inputs[0].value
+    const password = inputs[1].value
+    loginRequest(email,password)
+})
+
+async function loginRequest(email,password) {
+    fetch(
+        "http://localhost:5678/api/users/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                "accept":"application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }
+    ).then(response => {
+        if (!response.ok){
+            if (response.status == 401){
+                throw new Error(`mot de passe incorrecte !`)
+            }else if (response.status == 404){
+                throw new Error(`compte non existant !`)
+            }
+        }
+        return response.json()
+    }).then(data => {
+        if (data){
+            sessionStorage.setItem("token",JSON.stringify(data.token))
+            window.location.href = "./index.html"
+        }
+    }).catch(error =>{
+        document.querySelector(".msg-error").textContent = error.message
+    })
+}
