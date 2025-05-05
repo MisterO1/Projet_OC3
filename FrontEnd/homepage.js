eef1c44ad54e33458df74787e080e51107010c40
misterO <mistero4321@gmail.com>
2025-05-05 17:33:55 +0200

    feat : modal : load gallery works from api

--- FrontEnd/homepage.js
+++ FrontEnd/homepage.js
@@ -31,14 +31,35 @@ async function printWorks(category) {
 }
 
 // ****** modal : handle adding and deleting works ******* //
+const modal = document.querySelector(".modal");
+const modalBox = document.querySelector(".modal-box");
+
+modal.addEventListener("click", (e) => {
+    if (!modalBox.contains(e.target)) {
+    modal.classList.remove("active")
+    }
+})
+
 const editBtn = document.querySelector(".edit")
 editBtn.addEventListener("click", ()=>{
-    document.querySelector(".modal").classList.add("active")
+    const galleryWorks = document.querySelector(".gallery-works")
+    galleryWorks.innerHTML = ""
+    for (let work of works){
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
+        // const svg = document.createElement("svg")
+        img.src = work.imageUrl
+        img.alt = work.title
+        figure.appendChild(img)
+        figure.insertAdjacentHTML("beforeend", '<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect width="17" height="17" rx="2" fill="black"/> <path d="M6.71607 3.35558C6.82455 3.13661 7.04754 3 7.29063 3H9.70938C9.95246 3 10.1754 3.13661 10.2839 3.35558L10.4286 3.64286H12.3571C12.7127 3.64286 13 3.93013 13 4.28571C13 4.64129 12.7127 4.92857 12.3571 4.92857H4.64286C4.28728 4.92857 4 4.64129 4 4.28571C4 3.93013 4.28728 3.64286 4.64286 3.64286H6.57143L6.71607 3.35558ZM4.64286 5.57143H12.3571V12C12.3571 12.7092 11.7806 13.2857 11.0714 13.2857H5.92857C5.21942 13.2857 4.64286 12.7092 4.64286 12V5.57143ZM6.57143 6.85714C6.39464 6.85714 6.25 7.00179 6.25 7.17857V11.6786C6.25 11.8554 6.39464 12 6.57143 12C6.74821 12 6.89286 11.8554 6.89286 11.6786V7.17857C6.89286 7.00179 6.74821 6.85714 6.57143 6.85714ZM8.5 6.85714C8.32321 6.85714 8.17857 7.00179 8.17857 7.17857V11.6786C8.17857 11.8554 8.32321 12 8.5 12C8.67679 12 8.82143 11.8554 8.82143 11.6786V7.17857C8.82143 7.00179 8.67679 6.85714 8.5 6.85714ZM10.4286 6.85714C10.2518 6.85714 10.1071 7.00179 10.1071 7.17857V11.6786C10.1071 11.8554 10.2518 12 10.4286 12C10.6054 12 10.75 11.8554 10.75 11.6786V7.17857C10.75 7.00179 10.6054 6.85714 10.4286 6.85714Z" fill="white"/></svg>')
+        galleryWorks.appendChild(figure)
+    }
+    modal.classList.add("active")
 })
 
 const closeBtn = document.querySelector(".icon-close")
 closeBtn.addEventListener("click", ()=>{
-    document.querySelector(".modal").classList.remove("active")
+    modal.classList.remove("active")
 })
 
 const backBtn = document.querySelector(".icon-back")
@@ -54,9 +75,53 @@ addWorkBtn.addEventListener("click", ()=>{
     backBtn.classList.add("active")
 })
 
+// upload works
 const fileSelect = document.getElementById("fileSelect");
 const fileElem = document.getElementById("fileElem");
 
 fileSelect.addEventListener("click",() => {
     fileElem.click()
 })
+
+fileElem.addEventListener('change', function () {
+    const file = fileElem.files[0];
+    const error = document.querySelector(".upload-space-content p")
+    if (file) {
+        const maxSize = 1 * 1024 * 1024; // 4 Mo en octets
+        if (file.size > maxSize) {
+            error.textContent = "L'image ne doit pas dépasser 1 Mo.";
+            fileElem.value = ''; // Réinitialise le champ
+        } else {
+            error.textContent = "jpg, png : 4mo max"; // OK
+            document.querySelector(".upload-space-content").style.display = "none"
+            const reader = new FileReader();
+            const preview = document.querySelector("#preview")
+            reader.onload = function (e){
+                preview.src = e.target.result
+                preview.style.display = "block"
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+});
+
+const modalForm = document.querySelector(".form-works")
+const validateBtn = document.querySelector(".form-works > button")
+modalForm.addEventListener("change",()=>{
+    let resp = true
+    const listChamp = document.querySelectorAll(".form-works input,.form-works select")
+    for ( let champ of listChamp){
+        if (!champ.value){
+            resp = false
+            break
+        }
+    }
+    if (resp){
+        validateBtn.disabled = false
+        validateBtn.classList.add("active")
+    }else{
+        validateBtn.disabled = true
+        validateBtn.classList.remove("active")
+    }
+})
+
